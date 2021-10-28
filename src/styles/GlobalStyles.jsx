d72844f6cb41eeacc33b5d09a24ee55f46b7c138
Abdulsamii Ajala <kgasta@gmail.com>
2021-10-28 14:45:21 +0100

    global/root styling

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  body {
+    /* background: lightcoral; */
+  }
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyles;
