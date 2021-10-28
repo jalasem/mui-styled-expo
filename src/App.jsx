77f5119eca2179ecbb4ec9cef8bbc4a1aaa236aa
Abdulsamii Ajala <kgasta@gmail.com>
2021-10-28 15:19:47 +0100

    mui intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import Button from "./components/Button";
-import TomatoButton from "./components/Button/TomatoButton";
+import { Button, Autocomplete, TextField } from "@mui/material";
+import top100Films from "./mock/top100Films";
 
 const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
-      <Button>Click me</Button>
-      <Button primary>Sign up</Button>
-      <Button primary padding="0.75rem 1.5rem">
-        Register
-      </Button>
-      <TomatoButton>Add tomatoes</TomatoButton>
-      <Button primary>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-          />
-        </svg>
-        <span>Send now</span>
-      </Button>
-      <TomatoButton padding="0.75rem 1.5rem">Add tomatoes</TomatoButton>
-      <Button padding="1rem 2rem">Click me again</Button>
+      <Button>text button</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={top100Films}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Movie" />}
+      />
     </div>
   );
 };
--- src/components/Button/TomatoButton.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import Button from ".";
-
-const TomatoButton = styled(Button)`
-  background-color: tomato;
-  color: white;
-`;
-
-export default TomatoButton;
