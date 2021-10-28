d620d02633e54eb8daa467e0ea70b4338e04dd67
Abdulsamii Ajala <kgasta@gmail.com>
2021-10-28 14:32:11 +0100

    grouped prop-based styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
     <div>
       <h1>Hello World!</h1>
       <Button>Click me</Button>
+      <Button primary>Sign up</Button>
+      <Button primary padding="0.75rem 1.5rem">
+        Register
+      </Button>
       <Button padding="1rem 2rem">Click me again</Button>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,18 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   background: #ccd;
   color: #333;
   border: 0;
   border-radius: 5px;
+  margin: 0.5rem;
   padding: ${(props) => props.padding || "0.5rem 1rem"};
+  ${(props) =>
+    props.primary &&
+    css`
+      background: mediumseagreen;
+      color: white;
+    `}
   cursor: pointer;
 `;
 
