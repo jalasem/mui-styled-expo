2a2b3944121f4e13cedb667197c970cfd2f00541
Abdulsamii Ajala <kgasta@gmail.com>
2021-10-28 14:26:33 +0100

    feat: intro

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: #ccd;
+  color: #333;
+  border: 0;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+`;
+
+export default Button;
